790c8df2c913d9c00821e1709d3e0b97cc6fd5f4
KRILLE123 <42100456+KRILLE123@users.noreply.github.com>
2018-12-22 17:45:44 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,4 +1,5 @@
 local ill = false
+local chance = 6 -- The chance of being sick, checks every minute. 
 
 Citizen.CreateThread(function()
 
@@ -7,8 +8,8 @@ while true do
 
   local chanceill = math.random(1, 100)
 
-if chanceill < 6 then
-ill = true
+   if chanceill < 6 then
+       ill = true
 
     end
   end
@@ -16,41 +17,43 @@ end)
 
 
 Citizen.CreateThread(function()
-
 
 while true do
      local chansatthosta = math.random(30000, 100000)
+
 		Citizen.Wait(chansatthosta)
 
-    if ill then
+    if ill then --Checks if ill
 
-	RequestAnimDict("timetable@gardener@smoking_joint")
-   while not HasAnimDictLoaded("timetable@gardener@smoking_joint") do
-   	Citizen.Wait(100)
-   end
+--Cough animation
+	   RequestAnimDict("timetable@gardener@smoking_joint")
+         while not HasAnimDictLoaded("timetable@gardener@smoking_joint") do
+        	Citizen.Wait(100)
+         end
 
       TaskPlayAnim(GetPlayerPed(-1), "timetable@gardener@smoking_joint", "idle_cough", 8.0, 8.0, -1, 50, 0, false, false, false)
-      Citizen.Wait(3000)
-            ClearPedSecondaryTask(GetPlayerPed(-1))
+        Citizen.Wait(3000)
+      ClearPedSecondaryTask(GetPlayerPed(-1))
 
    end
   end
 end)
 
 Citizen.CreateThread(function()
-
-
 while true do
-     local chanstillfrisk = math.random(900000, 1800000)
+
+     local chanstillfrisk = math.random(900000, 1800000) -- Chance of being health by yourself
+
     Citizen.Wait(chanstillfrisk)
 
     if ill then
+    ill = false
 
-ill = false
     end
   end
 end)
 
+--antibiotics stuff 
 RegisterNetEvent('esx_kr_ill:frisk')
 AddEventHandler('esx_kr_ill:frisk', function()
   ill = false
